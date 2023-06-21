412dc5248875a3a36aec9089f6f3e8a90dc7e90b
youapp <saepudinjeh@gmail.com>
2023-06-21 21:11:23 +0700

    hover button social

--- src/components/buttons/social.button.jsx
+++ src/components/buttons/social.button.jsx
@@ -12,15 +12,15 @@ export default function ButtonSocial({ username = "Username", icon, rounded, typ
       };
 
     return (
-        <button onClick={handleRedirect} className={`${rounded ? 'rounded-full' : 'rounded-xl'} mx-auto px-4 py-1 bg-amber-400 w-fit flex items-center justify-center shadow shadow-amber-500`}>
+        <button onClick={handleRedirect} className={`${rounded ? 'rounded-full' : 'rounded-xl'} group mx-auto px-4 py-1 hover:bg-amber-200 bg-amber-400 duration-500 w-fit flex items-center justify-center shadow shadow-amber-500`}>
             <div className="flex items-center justify-center">
                 {icon ? (
                     <div className="flex items-center justify-center gap-x-2">
-                        <Icon icon={icon} />
-                        <span>{username}</span>
+                        <Icon className="group-hover:text-rose-700" icon={icon} />
+                        <span className="group-hover:text-amber-800">{username}</span>
                     </div>
                 ) : (
-                    <span>@{username}</span>
+                    <span className="group-hover:text-amber-800">@{username}</span>
                 )}
             </div>
         </button>
