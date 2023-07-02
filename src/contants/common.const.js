57e34eda0364c70a3f04b2816c376d31a2a31a40
youapp <saepudinjeh@gmail.com>
2023-07-02 17:53:20 +0700

    update constants

--- src/contants/common.const.js
+++ src/contants/common.const.js
@@ -8,4 +8,62 @@ export const SPREADSHEET_ID = "1z71V03f36yTH0KLANv3oLUyagLXOUu5LKPrTgcdNm6U";
 export const HOSTNAME_APP_SCRIPT = "https://script.google.com/macros/s/AKfycbxHOC8HqA1wVhdwVsuk-3NscyIya8qW4DnI3afFLke4B4CuKmTHgJ9wzU3zBsP5wnyrvg/exec";
 export const SHEET_NAME = "RSVP";
 
+
+// Welcome section
+export const REGARD_WELCOME = "Kepada Yth.";
+export const INVITE_TO_WELCOME = "Bapak/Ibu/Saudara/i"
+export const GREETING_WELCOME = "Assalamualaikum Warahmatullahi Wabarakatuh.";
+export const OPENING_WELCOME = "Dengan memohon anugerah dan berkat Tuhan, kami bermaksud mengumumkan kepada Bapak/Ibu/Saudara/i acara pernikahan kami:";
+export const CONJUNCTION_WELCOME = "dan";
+export const QUOTE_WELCOME = '"Demikianlah mereka bukan lagi dua, melainkan satu. Karena itu, apa yang telah dipersatukan Allah, tidak boleh diceraikan manusia."';
+export const VERSE_WELCOME = "Matius 19:6";
+
+// Couple section
+export const THE_BRIDE_COUPLE = "Sang Mempelai";
+export const FATHER_SON_COUPLE = "Putra dari Bapak";
+export const MOTHER_SON_COUPLE = "dan Ibu dari ";
+export const FATHER_DAUGHTER_COUPLE = "Putri dari Bapak";
+export const MOTHER_DAUGHTER_COUPLE = "dan dari Ibu";
+
+// Counting down setion
+export const DAY_COUNTING = "Hari";
+export const HOURS_COUNTING = "Jam";
+export const MINUTE_COUNTING = "Menit";
+export const SECOND_COUNTING = "Detik";
+export const REMIND_EVENT_COUNTING = "Ingatkan Acara";
+
+
+// Event section
+export const DETAIL_EVENT = "Detail Acara";
+export const MARRIAGE_CONTRACT_EVENT = "Akad Nikah";
+export const INVITE_EVENT = "Akad nikah akan dilaksanakan secara terbatas dan hanya dihadiri oleh keluarga dan kerabat dekat pada:";
+export const CLOCK_EVENT = "Pukul";
+export const FINISH_EVENT = "Smpai Selesai";
+
+// LIVE section
+export const BROADCAST_LIVE = "Siaran Langsung";
+export const START_LIVE = "Live pada";
+export const DISCLAIMER_LIVE = "Jika Anda menghadapi kesulitan saat memutar video live streaming di atas, mohon tekan tombol di bawah ini";
+export const TO_LIVE = "Menuju Youtube";
+
+// WISH AND PRAY (RSVP) Section
+export const WISH_AND_PRAY_RSVP = "Harapan dan Doa";
+export const RSVP_LABEL = "RSVP";
+export const NAME_LABEL_RSVP = "Nama";
+export const NAME_PLACEHOLDER_RSVP = "Masukan Namamu ...";
+export const WISH_LABEL_RSVP = "Ucapan & doa";
+export const WISH_PLACEHOLDER_RSVP = "Masukan ucapan atau doa ...";
+export const ATTENDANCE_LABEL_RSVP = "Kehadiran";
+export const PRESENT_LABEL_RSVP = "Akan hadir";
+export const SEND_RSVP = "Kirim ucapan";
+export const SEE_MORE_RSVP = "Lihat lebih lengkap";
+export const GREETINGS_RSVP = "Ucapan & doa dari yang terkasih";
+
+// PAYMENT section
+export const HEADING_PAYMENT = "Tanda Kasih";
+export const NOTE_PAYMENT = "Terima kasih atas harapan dan doanya dalam pesta pernikahan kami. Kami sangat menghargai restu dan kehadiranmu. Jika kamu berkeinginan untuk memberikan hadiah, kami ingin memudahkanmu dengan menyediakan opsi Amplop Digital. Terima kasih banyak.";
+export const LABEL_PAYMENT = "Pilih tujuan BANK transfer";
+export const TOAST_LABEL_PAYMENT = "berhasil disalin";
+
+// Footer
 export const SONG = "Virgoun - Bukti";
