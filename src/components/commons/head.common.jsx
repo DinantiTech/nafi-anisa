dd13a058d9d2d07a2aa055088bfefd962f17d3f8
youapp <saepudinjeh@gmail.com>
2023-07-30 20:29:35 +0700

    update head

--- src/components/commons/head.common.jsx
+++ src/components/commons/head.common.jsx
@@ -6,19 +6,19 @@ export default function HeadCommon() {
         <Helmet prioritizeSeoTags>
             <title>{TITLE_HEAD}</title>
 
-            {/* <link rel="icon" type="image/png" href={Favicon} /> */}
+            <link rel="shortcut icon" href="/vite.svg" type="image/x-icon" />
 
-            {/* <meta property="og:title" content={finalTitle} />
-            <meta property="og:image" content={SEO_IMAGE} />
-            <meta property="og:url" content={SEO_URL} />
-            <meta property="og:site_name" content={finalTitle} />
-            <meta property="og:description" content={finalDescription} />
-            <meta name="twitter:title" content={finalTitle} />
-            <meta name="twitter:description" content={finalDescription} />
-            <meta name="twitter:image" content={SEO_IMAGE} />
-            <meta name="twitter:url" content={SEO_URL} /> */}
-            {/* <meta name="twitter:card" content="summary" />
-            <meta name="twitter:creator" content={CREATORTHEME} /> */}
+            {/* <meta property="og:title" content={finalTitle} /> */}
+            {/* <meta property="og:image" content={SEO_IMAGE} /> */}
+            {/* <meta property="og:url" content={SEO_URL} /> */}
+            {/* <meta property="og:site_name" content={finalTitle} /> */}
+            {/* <meta property="og:description" content={finalDescription} /> */}
+            {/* <meta name="twitter:title" content={finalTitle} /> */}
+            {/* <meta name="twitter:description" content={finalDescription} /> */}
+            {/* <meta name="twitter:image" content={SEO_IMAGE} /> */}
+            {/* <meta name="twitter:url" content={SEO_URL} /> */}
+            {/* <meta name="twitter:card" content="summary" /> */}
+            {/* <meta name="twitter:creator" content={CREATORTHEME} /> */}
 
             <link rel="preconnect" href="https://fonts.googleapis.com" />
             <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
