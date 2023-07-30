e76c06992c6b335941dad95a58ac560e5d8f6cf4
youapp <saepudinjeh@gmail.com>
2023-07-30 20:21:19 +0700

    add head

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,14 @@ import ReactDOM from 'react-dom/client'
 import './styles/index.css'
 import { RouterProvider } from 'react-router-dom'
 import { router } from './routes'
+import { HelmetProvider } from 'react-helmet-async'
+import HeadCommon from './components/commons/head.common'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <HelmetProvider>
+      <HeadCommon />
       <RouterProvider router={router} />
+    </HelmetProvider>
   </React.StrictMode>,
 )
