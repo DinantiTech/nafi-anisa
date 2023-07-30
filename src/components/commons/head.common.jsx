9d1f6cee4731c64b1b39f867093d7af87d1efdd9
youapp <saepudinjeh@gmail.com>
2023-07-30 20:42:27 +0700

    update head

--- src/components/commons/head.common.jsx
+++ src/components/commons/head.common.jsx
@@ -1,7 +1,10 @@
 import { Helmet } from "react-helmet-async";
-import { SEO_IMAGE, SEO_TITLE_HEAD } from "../../contants/head.const";
+import { SEO_CREATORTHEME, SEO_DESC, SEO_IMAGE, SEO_TITLE_HEAD } from "../../contants/head.const";
 
 export default function HeadCommon() {
+
+    const CURRENT_URL = window.location.href;
+
     return (
         <Helmet prioritizeSeoTags>
             <title>{SEO_TITLE_HEAD}</title>
@@ -10,15 +13,15 @@ export default function HeadCommon() {
 
             <meta property="og:title" content={SEO_TITLE_HEAD} />
             <meta property="og:image" content={SEO_IMAGE} />
-            {/* <meta property="og:url" content={SEO_URL} /> */}
-            {/* <meta property="og:site_name" content={finalTitle} /> */}
-            {/* <meta property="og:description" content={finalDescription} /> */}
-            {/* <meta name="twitter:title" content={finalTitle} /> */}
-            {/* <meta name="twitter:description" content={finalDescription} /> */}
-            {/* <meta name="twitter:image" content={SEO_IMAGE} /> */}
-            {/* <meta name="twitter:url" content={SEO_URL} /> */}
-            {/* <meta name="twitter:card" content="summary" /> */}
-            {/* <meta name="twitter:creator" content={CREATORTHEME} /> */}
+            <meta property="og:url" content={CURRENT_URL} />
+            <meta property="og:site_name" content={SEO_TITLE_HEAD} />
+            <meta property="og:description" content={SEO_DESC} />
+            <meta name="twitter:title" content={SEO_TITLE_HEAD} />
+            <meta name="twitter:description" content={SEO_DESC} />
+            <meta name="twitter:image" content={SEO_IMAGE} />
+            <meta name="twitter:url" content={CURRENT_URL} />
+            <meta name="twitter:card" content="summary" />
+            <meta name="twitter:creator" content={SEO_CREATORTHEME} />
 
             <link rel="preconnect" href="https://fonts.googleapis.com" />
             <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
