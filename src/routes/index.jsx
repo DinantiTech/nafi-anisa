9e672fc2d041a91127052763a7c59a3039bf1cdf
youapp <saepudinjeh@gmail.com>
2023-09-27 00:36:59 +0700

    removed route story

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,14 +1,9 @@
 import { createBrowserRouter } from "react-router-dom";
 import MainPage from "../pages/main.page";
-import Stories from "../pages/story.page";
 
 export const router = createBrowserRouter([
   {
     path: "/",
     element: <MainPage />,
   },
-  {
-    path: '/stories',
-    element: <Stories />
-  }
 ]);
