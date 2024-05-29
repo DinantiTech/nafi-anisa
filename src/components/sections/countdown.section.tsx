ffef42c07daa9d34d8017d31f0cffb575d196539
wkwkw <saepudinjeh@gmail.com>
2024-05-29 22:56:17 +0700

    adjust stories

--- src/components/sections/countdown.section.tsx
+++ src/components/sections/countdown.section.tsx
@@ -19,28 +19,28 @@ export default function CountdownSection() {
                     <div className="absolute top-0 left-0 z-20 w-full h-full flex flex-col items-center justify-center xxs:gap-y-2">
                         <div className="flex flex-col items-center justify-center xxs:gap-y-2">
                             <p className="font-light xxs:text-xl tracking-wider">COUNT DOWN</p>
-                            <h2 className="font-medium xxs:text-4xl text-2xl tracking-widest text-center">OUR SPECIAL DAYS</h2>
+                            <h2 className="font-medium sm:text-4xl xxs:text-3xl text-2xl tracking-widest text-center">OUR SPECIAL DAYS</h2>
                         </div>
                         <div className="flex items-center justify-center xxs:gap-6 gap-3">
 
                             <div className="xs:w-16 w-14 font-semibold cursor-pointer rounded-xl text-center xs:py-2 py-1 leading-snug text-white">
-                                <h5 className="xs:text-2xl text-sm font-normal">{days}</h5>
-                                <p className="text-sm xs:text-base sm:text-xl tracking-wide">Days</p>
+                                <h5 className="xxs:text-2xl text-sm font-normal">{days}</h5>
+                                <p className="text-sm xxs:text-base sm:text-xl tracking-wide">Days</p>
                             </div>
 
                             <div className="xs:w-16 w-14 font-semibold cursor-pointer rounded-xl text-center xs:py-2 py-1 leading-snug text-white">
-                                <h5 className="xs:text-2xl text-sm font-normal">{hours}</h5>
-                                <p className="text-sm xs:text-base sm:text-xl tracking-wide">Hours</p>
+                                <h5 className="xxs:text-2xl text-sm font-normal">{hours}</h5>
+                                <p className="text-sm xxs:text-base sm:text-xl tracking-wide">Hours</p>
                             </div>
 
                             <div className="xs:w-16 w-14 font-semibold cursor-pointer rounded-xl text-center xs:py-2 py-1 leading-snug text-white">
-                                <h5 className="xs:text-2xl text-sm font-normal">{minutes}</h5>
-                                <p className="text-sm xs:text-base sm:text-xl tracking-wide">Minutes</p>
+                                <h5 className="xxs:text-2xl text-sm font-normal">{minutes}</h5>
+                                <p className="text-sm xxs:text-base sm:text-xl tracking-wide">Minutes</p>
                             </div>
 
                             <div className="xs:w-16 w-14 font-semibold cursor-pointer rounded-xl text-center xs:py-2 py-1 leading-snug text-white">
-                                <h5 className="xs:text-2xl text-sm font-normal">{seconds}</h5>
-                                <p className="text-sm xs:text-base sm:text-xl tracking-wide">Seconds</p>
+                                <h5 className="xxs:text-2xl text-sm font-normal">{seconds}</h5>
+                                <p className="text-sm xxs:text-base sm:text-xl tracking-wide">Seconds</p>
                             </div>
                         </div>
                     </div>
--- src/components/sections/stories.section.tsx
+++ src/components/sections/stories.section.tsx
@@ -9,7 +9,7 @@ export default function StoriesSection() {
             <div className="flex flex-col items-center justify-center text-black mt-14 gap-y-10 md:gap-y-14 h-fit">
                 <HeadingTitle title='OUR STORY' />
                 {Stories?.map((data, idx) => (
-                    <div key={data.chapter} className="relative flex items-center justify-center h-full gap-y-40">
+                    <div key={data.chapter} className="relative flex items-center justify-center h-full gap-y-40 w-full">
                         <HeadingChapter isRight={idx % 2 === 0} title={data.chapter} />
                         <ImageStory isRight={idx % 2 === 0} ImgUrl={data.image} story={data.story} />
                     </div>
@@ -22,25 +22,25 @@ export default function StoriesSection() {
 function ImageStory({ story, ImgUrl, isRight }: { story: string, ImgUrl: StaticImageData, isRight: boolean }) {
     const orderPositionImg = !isRight ? "order-last" : "-order-1";
     const orderPosition = isRight ? "order-last" : "-order-1";
-    const marginPosition = isRight ? "ml-0 mr-1 xxs:mr-2 md:mr-5" : "ml-0 ml-1 xxs:ml-2 md:ml-5";
+    const marginPosition = isRight ? "pr-8" : "pl-8";
 
     return (
-        <div className={`${orderPosition} w-[80%] flex flex-col gap-x-5 xxs:gap-y-4 gap-y-2 items-center justify-center h-full`}>
-            <div className="flex items-center justify-center">
+        <div className={`${orderPosition} w-[70%] flex flex-col gap-x-5 xxs:gap-y-4 gap-y-2 items-center justify-center h-full`}>
+            <div className={`${marginPosition} flex items-center justify-center`}>
                 <div data-aos="fade-down" className={`${orderPositionImg} divider divider-horizontal divider-neutral`} />
                 <Image src={ImgUrl} alt={story} sizes="100vw" />
             </div>
-            <p data-aos="fade-down" className={` text-xs xxs:text-sm sm:text-base text-center px-2`}>{story}</p>
+            <p data-aos="fade-down" className={`px-3 text-xs xxs:text-sm sm:text-base text-center`}>{story}</p>
         </div>
     );
 }
 
 function HeadingChapter({ title, isRight }: { title: string, isRight: boolean }) {
-    const orderPosition = isRight ? "xs:-left-24 xxs:-left-16 -left-[5rem]" : "xs:-right-24 xxs:-right-16 -right-[5rem]";
+    const orderPosition = isRight ? "md:-left-14 xs:-left-24 xxs:-left-14 -left-[4rem]" : "md:-right-14 xs:-right-24 xxs:-right-14 -right-[4rem]";
     const transtitionAnimation = isRight ? "fade-down": "fade-up"
     return (
-        <div className="h-full w-[20%]">
-            <div className={`${orderPosition} top-48 sm:top-56 lg:top-64 absolute flex items-center justify-start -rotate-90 font-bold text-4xl xs:text-5xl tracking-widest`}>
+        <div className="h-full w-[30%]">
+            <div className={`${orderPosition} top-32 xxs:top-40 xs:top-52 sm:top-56 absolute flex items-center justify-start -rotate-90 font-bold text-4xl xs:text-5xl tracking-widest`}>
                 <p data-aos={transtitionAnimation} className="w-full h-full">{title.toUpperCase()}</p>
             </div>
         </div>
