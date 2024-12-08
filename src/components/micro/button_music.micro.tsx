b32459b050dd324b8f3b4f50fb7a60f4986b2410
wkwkw <saepudinjeh@gmail.com>
2024-12-08 16:55:01 +0700

    fix issue document not found

--- src/components/micro/button_music.micro.tsx
+++ src/components/micro/button_music.micro.tsx
@@ -3,29 +3,44 @@
 import { MusicBG } from "@/libs/constants/others.const";
 import { useMusicStore } from "@/libs/stores/music.store";
 import { Icon } from "@iconify/react";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 export default function MusicBtn() {
   const { isMusic, setMusic } = useMusicStore();
+  const [audio, setAudio] = useState<HTMLAudioElement | null>(null);
 
   useEffect(() => {
-    const audio = document.getElementById('audio') as HTMLAudioElement;
+    // Only run this code in the browser
+    const audioElement = new Audio(MusicBG);
+    audioElement.loop = true;
+    setAudio(audioElement);
 
-    if (isMusic) {
-      audio.play();
-    } else {
-      audio.pause();
+    return () => {
+      audioElement.pause();
+      audioElement.currentTime = 0; // Reset audio
+    };
+  }, []);
+
+  useEffect(() => {
+    if (audio) {
+      if (isMusic) {
+        audio.play();
+      } else {
+        audio.pause();
+      }
     }
-  }, [isMusic]);
+  }, [isMusic, audio]);
 
   return (
-    <button onClick={() => setMusic(!isMusic)} className={`${isMusic ? "animate-bounce" : ""} bg-white fixed right-5 xxs:right-10 bottom-16 z-50 p-2 text-xl xxs:text-3xl rounded-full shadow-md`}>
-      <audio id="audio" src={MusicBG} loop />
-      { isMusic ? (
+    <button
+      onClick={() => setMusic(!isMusic)}
+      className={`${isMusic ? "animate-bounce" : ""} bg-white fixed right-5 xxs:right-10 bottom-16 z-50 p-2 text-xl xxs:text-3xl rounded-full shadow-md`}
+    >
+      {isMusic ? (
         <Icon icon="icon-park-twotone:pause-one" />
       ) : (
         <Icon icon="icon-park-twotone:play" />
-      ) }
+      )}
     </button>
   );
 }
