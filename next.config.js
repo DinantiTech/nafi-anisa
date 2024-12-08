2a608db358e6c808714ff9414e64df6ce99e987e
wkwkw <saepudinjeh@gmail.com>
2024-12-08 17:10:14 +0700

    test deploy

--- next.config.js
+++ next.config.js
@@ -1,11 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    compiler: {
-        removeConsole: process.env.NODE_ENV === 'production',
-    },
-    experimental: {
-        typedRoutes: true
-    },
+    // compiler: {
+    //     removeConsole: process.env.NODE_ENV === 'production',
+    // },
+    // experimental: {
+    //     typedRoutes: true
+    // },
     compress: true,
 }
 
