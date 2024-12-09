1701b5c2a6cfc2dc150110ab7de86c4809719f03
wkwkw <saepudinjeh@gmail.com>
2024-12-09 20:51:58 +0700

    test opengraph image

---
+++ src/app/opengraph-image.tsx
@@ -0,0 +1,26 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+/* eslint-disable @next/next/no-img-element */
+/* eslint-disable jsx-a11y/alt-text */
+import { ImageResponse } from 'next/og'
+ 
+export const runtime = 'edge'
+ 
+export default async function Image() {
+  const logoSrc: any = await fetch(new URL('./img-opengraph.png', import.meta.url)).then(
+    (res) => res.arrayBuffer()
+  )
+ 
+  return new ImageResponse(
+    (
+      <div
+        style={{
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'center',
+        }}
+      >
+        <img src={logoSrc} height="100" />
+      </div>
+    )
+  )
+}
