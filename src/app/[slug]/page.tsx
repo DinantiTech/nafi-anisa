6889a9efa791cb5e9846675a5addf72fb8bf600f
wkwkw <dinantitech@gmail.com>
2024-12-15 17:11:47 +0700

    dinamis title meta

--- src/app/[slug]/page.tsx
+++ src/app/[slug]/page.tsx
@@ -1,9 +1,18 @@
+import { NEWLYWEDS_TITLE } from "@/components/libs/constants/others.const";
 import { Suspense, lazy } from "react";
 
 const IndexComponent = lazy(() => import("@/components/index.component"));
 
 type SlugType = {
     params: Promise<{ slug: string }>
+}
+
+export async function generateMetadata({ params }: SlugType) {
+  const slug =  (await params).slug
+  
+  return {
+    title: `${NEWLYWEDS_TITLE} - To ${slug}`,
+  }
 }
 
 export default async function Slug({ params }: SlugType) {
