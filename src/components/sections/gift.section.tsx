306567650287dcc8db7344f673816e6ff2bcae5a
danes44 <frumentius44@gmail.com>
2025-01-13 21:50:58 +0700

    tweak: remove gift card copy button border

--- src/components/sections/gift.section.tsx
+++ src/components/sections/gift.section.tsx
@@ -79,7 +79,7 @@ export default function GiftSection() {
                                     <p className="font-semibold text-lg">{data?.value}</p>
                                     <p className="italic">{data?.beneficiary}</p>
                                 </div>
-                                <button onClick={() => copyToClipboard({ value: data?.value, type: "REK" })} className="btn btn-sm sm:btn-md text-white flex items-center justify-center bg-[#E8B787] hover:bg-[#E8B787]/90 font-normal">
+                                <button onClick={() => copyToClipboard({ value: data?.value, type: "REK" })} className="btn btn-sm sm:btn-md text-white flex items-center justify-center bg-[#E8B787] hover:bg-[#E8B787]/90 border-0 font-normal">
                                     <Icon className="text-lg" icon="solar:copy-line-duotone" />
                                     COPY
                                 </button>
