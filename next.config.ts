a904a062d9261c10ab49f393e7798df969f76d48
agung saepudin <dinantitech@gmail.com>
2025-02-28 09:12:39 +0700

    test deploy 2

--- next.config.ts
+++ next.config.ts
@@ -1,20 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  webpack: (config) => {
-    config.module.rules.push({
-      test: /\.(ttf|woff|woff2|eot)$/,
-      use: {
-        loader: 'file-loader',
-        options: {
-          publicPath: '/_next/static/fonts/',
-          outputPath: 'static/fonts/',
-          name: '[name].[ext]',
-        },
-      },
-    });
-    return config;
-  },
   /* config options here */
   compress: true
 };
