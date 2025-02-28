162a0840079c4e52fef2f83b9badd1e7f85e9904
agung saepudin <dinantitech@gmail.com>
2025-02-28 09:02:52 +0700

    added config next js

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,20 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.(ttf|woff|woff2|eot)$/,
+      use: {
+        loader: 'file-loader',
+        options: {
+          publicPath: '/_next/static/fonts/',
+          outputPath: 'static/fonts/',
+          name: '[name].[ext]',
+        },
+      },
+    });
+    return config;
+  },
   /* config options here */
   compress: true
 };
